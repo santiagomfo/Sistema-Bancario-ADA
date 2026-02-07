5f145e00e6604aa340b77edc750963ccb1a3c78e
Bryan Guano <72318149+Bryan-0220@users.noreply.github.com>
2026-02-07 12:51:17 -0500

    feat: implement Aplicar_Interes procedure in Cuentas_Ahorro_Service package

---
+++ src/logic/cuentas/cuentas_ahorro_service.ads
@@ -0,0 +1,8 @@
+with Cuenta_Ahorros;
+with Cuentas;
+
+package Cuentas_Ahorro_Service is
+
+   procedure Aplicar_Interes (Cuenta : in out Cuenta_Ahorros.Cuenta_Ahorros_Type);
+
+end Cuentas_Ahorro_Service;
